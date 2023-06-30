f6f6276e1b461e3fc760f8ca7c3e5092ae863ced
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2023-07-01 01:50:34 +0530

    Merge Sort

---
+++ 88. Merge Sort.py
@@ -0,0 +1,17 @@
+def merge(left, right):
+    merged = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    # Append the remaining elements
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+
+    return merged
